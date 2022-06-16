130a79bee0006a3e43cb13c1184b55e295c92efc
FlyWheel <qq844723532@gmail.com>
2022-06-16 17:16:42 +0800

    init

---
+++ choo1/src/main/java/com/flywheel/choo1/Choo1Application.java
@@ -0,0 +1,13 @@
+package com.flywheel.choo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Choo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Choo1Application.class, args);
+	}
+
+}
---
+++ choo1/src/test/java/com/flywheel/choo1/Choo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flywheel.choo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Choo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
